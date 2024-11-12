fdf86216da3aac2c2f406798512b5f39a90e7895
Jhon Mario Lotero <jhoning.soft@gmail.com>
2024-11-11 23:00:41 -0500

    * share multiple values

--- dags/python_dag.py
+++ dags/python_dag.py
@@ -12,14 +12,21 @@ default_args = {
 """
 ti = task instance
 """
-def greet(age, ti):
-    print(f"Hello World from Python Function! {ti.xcom_pull(task_ids='get_name')}, {age}")
+def greet(ti):
+    name = ti.xcom_pull(key="name", task_ids=["get_name"])
+    last_name = ti.xcom_pull(key="last_name", task_ids=["get_name"])
+    age = ti.xcom_pull(key="age", task_ids=["get_age"])
+    print(f"Hello {name} {last_name} World from Python Function!, your age is = {age}")
 
-def get_name():
-    return "Jhon"
+def get_name(ti):
+    ti.xcom_push(key="name", value="Jhon")
+    ti.xcom_push(key="last_name", value="Lotero")
+
+def get_age(ti):
+    ti.xcom_push(key="age", value=27)
 
 with DAG(
-    dag_id="first_python_dag_v2",
+    dag_id="first_python_dag_v6",
     description="This is my first Python DAG",
     default_args=default_args,
     start_date=datetime(2024, 11, 10),
@@ -36,8 +43,11 @@ with DAG(
     task_3 = PythonOperator(
         task_id="task_3", 
         python_callable=greet,
-        op_kwargs={"age": 27} # pass the arguments to the function
+        #op_kwargs={"age": 27} # pass the arguments to the function
+    )
+    task_4 = PythonOperator(
+        task_id="get_age", 
+        python_callable=get_age
     )
 
-    # 
-    task_2 >> [task_1,task_3]
+    [task_2, task_4] >> task_1 >> task_3
